fc882db9201752d90b1702e58bfaaed9bcd47566
Dapple1990 <bone.dalma@gmail.com>
2019-10-08 15:54:58 +0200

    fix(): add class for css

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -4,12 +4,13 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
 const Menu = () => (
-  <div>
+  <div className="nav-container">
     <Navbar className="navbar-opacity" bg="light" variant="light">
       <Nav className="mr-auto">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/coffee">Facts</NavLink>
         <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/cupOf">Cup of coffee</NavLink>
       </Nav>
     </Navbar>
   </div>
