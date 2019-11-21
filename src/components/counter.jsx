d5a981a89cd2ae4e944d9bcc3c746019c9bf5664
dapple1990 <bone.dalma@gmail.com>
2019-11-21 17:15:38 +0100

    feat(): add hooks to component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,53 +1,46 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+
 
 export const RECIPE_CORTADO = 'Cortado';
 export const RECIPE_ESPRESSO = 'Espresso';
 export const RECIPE_FLATWHITE = 'Flat White';
 
 export function Counter({ type, amount }) {
-
-  const ingredients = () => {
-    let waterRatio, milkRatio, coffeeRatio = 0;
+  const [{ waterRatio, milkRatio, coffeeRatio }, setRatios] =
+    useState({ waterRatio: 0, milkRatio: 0, coffeeRatio: 0 })
+  useEffect(() => {
     switch (type) {
       case RECIPE_CORTADO:
-        waterRatio = 2;
-        milkRatio = 4;
-        coffeeRatio = 3;
+        setRatios(() => ({ waterRatio: 2, milkRatio: 4, coffeeRatio: 3 }));
         break;
       case RECIPE_ESPRESSO:
-        waterRatio = 2;
-        milkRatio = 0;
-        coffeeRatio = 2;
+        setRatios(() => ({ waterRatio: 2, milkRatio: 0, coffeeRatio: 2 }));
         break;
       case RECIPE_FLATWHITE:
-        waterRatio = 2;
-        milkRatio = 4;
-        coffeeRatio = 3;
+        setRatios(() => ({ waterRatio: 2, milkRatio: 4, coffeeRatio: 4 }));
         break;
       default:
         console.log('Drink coffee be fluffy')
     }
-    let water = amount * waterRatio;
-    let milk = amount * milkRatio;
-    let coffee = amount * coffeeRatio;
-    return (
-      <div>
-        <p className="water">Water {water}</p>
-        <p className="coffee">Coffee {coffee}</p>
-        <p className="milk">Milk {milk}</p>
-      </div>
-    )
-  }
+
+  }, [type]);
+
+  let water = amount * waterRatio;
+  let milk = amount * milkRatio;
+  let coffee = amount * coffeeRatio;
 
   return (
     <div className="input-container">
       <span role="img" aria-label="Coffee">
         ☕
         </span>
-      {ingredients()}
+      <div>
+        <p className="water">Water {water}</p>
+        <p className="coffee">Coffee {coffee}</p>
+        <p className="milk">Milk {milk}</p>
+      </div>
     </div>
   );
 }
-
 
 export default Counter;
