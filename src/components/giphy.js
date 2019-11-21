7bc00d4fe18e4cf4975ecb9ab24ec780eda9da59
dapple1990 <bone.dalma@gmail.com>
2019-11-21 17:23:22 +0100

    feat(): add hooks to component

--- src/components/giphy.js
+++ src/components/giphy.js
@@ -1,47 +1,37 @@
-import React, { Component } from "react";
-import Image from "react-bootstrap/Image";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import SupportButton from "./supportFlow";
+import Image from "react-bootstrap/Image";
 
 
+function Giphy() {
 
-class Giphy extends Component {
-  state = {
-    giphys: []
-  };
+  const [giphy, setGiphy] = useState('');
 
-  componentDidMount() {
-    axios
-      .get(
-        "https://cors-anywhere.herokuapp.com/https://coffee.alexflipnote.dev/random.json?fbclid=IwAR2FKFn5oty4UzpGvdVVEljHeCdjfPlCgYemKIAu2eYd66HtrPfC2FmX_6M"
-      )
-      .then(res => {
-        const giphys = [];
-        giphys.push(res.data);
-        this.setState({ giphys });
-      });
-  }
+  useEffect(() => {
+    const getData = async () => {
+      await axios
+        .get(
+          "https://cors-anywhere.herokuapp.com/https://coffee.alexflipnote.dev/random.json?fbclid=IwAR2FKFn5oty4UzpGvdVVEljHeCdjfPlCgYemKIAu2eYd66HtrPfC2FmX_6M"
+        ).then(res => setGiphy(res.data.file));
+    };
+    getData()
+  }, [])
 
-  render() {
-    let coffeGiphy = this.state.giphys.map(giphy => {
-      return (
+  return (
+    <div className="giphy-box">
+      <h2 className="giphy-header">This is a random coffee image!</h2>
+      {giphy &&
         <Image
           className="img-giphy"
-          key={giphy}
-          src={giphy.file}
-          alt=""
-          roundedCircle
-        />
-      );
-    });
-    return (
-      <div className="giphy-box">
-        <h2 className="giphy-header">This is a random coffee image!</h2>
-        {coffeGiphy}
-       <SupportButton/>
-      </div>
-    );
-  }
+          src={giphy}
+          alt="random coffe image"
+          roundedCircle />
+      }
+      <SupportButton />
+    </div>
+  );
 }
+
 
 export default Giphy;
